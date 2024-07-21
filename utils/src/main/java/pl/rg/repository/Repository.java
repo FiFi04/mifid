78e03b478fb808a81963ccb257ec681496b250c0
Rafal <rafalgrabias@smartoakprojects.com>
2024-07-21 23:59:47 +0200

    MIFID-2 update

---
+++ utils/src/main/java/pl/rg/repository/Repository.java
@@ -0,0 +1,27 @@
+package pl.rg.repository;
+
+import pl.rg.db.DBConnector;
+import pl.rg.repository.model.MifidGeneral;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public interface Repository<T extends MifidGeneral<E>, E> {
+
+    DBConnector dbConnector = DBConnector.getInstance();
+
+    List<T> findAll() throws SQLException;
+
+    Optional<T> findById(E id) throws SQLException;
+
+    Optional<T> findFetchById(E id) throws SQLException;
+
+    void deleteAll(Iterable<T> objects) throws SQLException;
+
+    void deleteById(E id) throws SQLException;
+
+    void save(T object) throws SQLException;
+
+    T saveAndFlush(T object) throws SQLException;
+}
