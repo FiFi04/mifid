1bc87050cfbccaa3ab6e2b09a2e2af1eb393b39d
Rafal <rafalgrabias@smartoakprojects.com>
2024-07-22 01:41:00 +0200

    MIFID-1 update

---
+++ utils/src/main/java/pl/rg/db/DBConnector.java
@@ -0,0 +1,54 @@
+package pl.rg.db;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnector {
+
+    private static DBConnector dbConnector;
+
+    private Connection connection;
+
+    private DBConnector() {
+        this.connection = initializeConnection();
+    }
+
+    public static DBConnector getInstance() {
+        if (dbConnector == null) {
+            dbConnector = new DBConnector();
+        }
+        return dbConnector;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    private Connection initializeConnection() {
+        try {
+            Properties properties = getProperties();
+//            String driverClassName = properties.getProperty("db.driverClassName");
+            String url = properties.getProperty("db.url");
+            String username = properties.getProperty("db.username");
+            String password = properties.getProperty("db.password");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+    private Properties getProperties() throws IOException {
+        Properties properties = new Properties();
+        try (InputStream inputStream = DBConnector.class.getClassLoader().getResourceAsStream("app.properties")) {
+            properties.load(inputStream);
+        }
+        return properties;
+    }
+}
