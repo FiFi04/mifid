e2be7c585dfee4fb47360944e0ca9b7972fe761d
Mateusz Tadeusiak <mateusz.tadeusiak@eviden.com>
2024-11-18 20:12:00 +0100

    MIFID-14: simple fixes

--- users/users-impl/src/main/java/pl/rg/users/mapper/UserMapper.java
+++ users/users-impl/src/main/java/pl/rg/users/mapper/UserMapper.java
@@ -1,7 +1,5 @@
 package pl.rg.users.mapper;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.ObjectFactory;
 import org.mapstruct.factory.Mappers;
@@ -9,10 +7,11 @@ import pl.rg.users.User;
 import pl.rg.users.UserDto;
 import pl.rg.users.impl.UserImpl;
 import pl.rg.users.model.UserModel;
+import pl.rg.utils.pageAndSort.MifidPageMapper;
 import pl.rg.utils.repository.MifidPage;
 
 @Mapper
-public interface UserMapper {
+public interface UserMapper extends MifidPageMapper {
 
   UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
@@ -29,47 +28,7 @@ public interface UserMapper {
     return new UserImpl();
   }
 
-  default MifidPage<User> userModelPageToUserPage(MifidPage<UserModel> userModelPage) {
-    MifidPage<User> userPage = new MifidPage<>(userModelPage.getTotalObjects(),
-        userModelPage.getTotalPage(), userModelPage.getObjectFrom(), userModelPage.getObjectTo(),
-        new ArrayList<>());
-    List<User> mappedUsers = userModelPage.getLimitedObjects().stream()
-        .map(this::userModelToDomain)
-        .toList();
-    userPage.setLimitedObjects(mappedUsers);
-    return userPage;
-  }
+  MifidPage<User> userModelPageToUserPage(MifidPage<UserModel> userModelPage);
 
-  default MifidPage<UserDto> userPageToUserDtoPage(MifidPage<User> userPage) {
-    MifidPage<UserDto> userDtoPage = new MifidPage<>(userPage.getTotalObjects(),
-        userPage.getTotalPage(), userPage.getObjectFrom(), userPage.getObjectTo(),
-        new ArrayList<>());
-    List<UserDto> mappedUsers = userPage.getLimitedObjects().stream()
-        .map(this::domainToDto)
-        .toList();
-    userDtoPage.setLimitedObjects(mappedUsers);
-    return userDtoPage;
-  }
-
-  default MifidPage<UserModel> userPageToUserModelPage(MifidPage<User> userPage) {
-    MifidPage<UserModel> userModelPage = new MifidPage<>(userPage.getTotalObjects(),
-        userPage.getTotalPage(), userPage.getObjectFrom(), userPage.getObjectTo(),
-        new ArrayList<>());
-    List<UserModel> mappedUsers = userPage.getLimitedObjects().stream()
-        .map(this::domainToUserModel)
-        .toList();
-    userModelPage.setLimitedObjects(mappedUsers);
-    return userModelPage;
-  }
-
-  default MifidPage<User> userDtoPageToUserPage(MifidPage<UserDto> userDtoPage) {
-    MifidPage<User> userPage = new MifidPage<>(userDtoPage.getTotalObjects(),
-        userDtoPage.getTotalPage(), userDtoPage.getObjectFrom(), userDtoPage.getObjectTo(),
-        new ArrayList<>());
-    List<User> mappedUsers = userDtoPage.getLimitedObjects().stream()
-        .map(this::dtoToDomain)
-        .toList();
-    userPage.setLimitedObjects(mappedUsers);
-    return userPage;
-  }
+  MifidPage<UserDto> userPageToUserDtoPage(MifidPage<User> userPage);
 }
