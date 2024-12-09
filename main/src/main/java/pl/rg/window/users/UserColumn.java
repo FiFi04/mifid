38a6df2b71fced4c58cd23d2c06a26cc6ccbd925
Rafal <rafalgrabias@smartoakprojects.com>
2024-12-09 20:48:29 +0100

    MIFID-21 + fixes

--- main/src/main/java/pl/rg/window/users/UserColumn.java
+++ main/src/main/java/pl/rg/window/users/UserColumn.java
@@ -1,15 +1,14 @@
 package pl.rg.window.users;
 
-import java.util.Arrays;
-import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import pl.rg.users.model.UserModel;
+import pl.rg.window.DataEnumColumn;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public enum UserColumn {
+public enum UserColumn implements DataEnumColumn {
 
   ID("Id", UserModel.ID, false, "id"),
   LOGIN("Login", UserModel.USER_NAME, false, "userName"),
@@ -19,28 +18,9 @@ public enum UserColumn {
 
   private String name;
 
-  private String dbColum;
+  private String dbColumn;
 
   private boolean visibility;
 
   private String javaAttribute;
-
-  public static String[] getColumnNames() {
-    return Arrays.stream(values())
-        .map(u -> u.getName())
-        .toArray(size -> new String[size]);
-  }
-
-  public static String getNameByJavaAttribute(String javaAttribute) {
-    return Arrays.stream(values())
-        .filter(v -> v.getJavaAttribute().equals(javaAttribute))
-        .findFirst().get().getName();
-  }
-
-  public static Optional<String> getDbColumnByName(String name) {
-    return Arrays.stream(values())
-        .filter(column -> column.name.equals(name))
-        .map(UserColumn::getDbColum)
-        .findFirst();
-  }
 }
